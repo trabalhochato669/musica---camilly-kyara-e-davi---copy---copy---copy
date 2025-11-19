252afe948c8443d39bcebe22a69d7dcba7368887
trabalhochato669 <trabalho.chato669@gmail.com>
2025-11-19 11:01:22 -0300

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
 forever(function () {
+    music.play(music.createSong(assets.song`camilly e Kyara0`), music.PlaybackMode.UntilDone)
+    music.play(music.createSong(assets.song`Camilly e Kyara`), music.PlaybackMode.UntilDone)
+})
+forever(function () {
     music.play(music.createSong(assets.song`egito`), music.PlaybackMode.UntilDone)
+    music.play(music.createSong(assets.song`Camilly e Kyara`), music.PlaybackMode.UntilDone)
 })
